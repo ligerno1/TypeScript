a4755317ff67d880c2a8d60144a95891f86d1ebc
liger1992 <359676921@qq.com>
2021-07-21 18:46:00 +0800

    Delete TypeScript/精通/demo/dist directory

--- TypeScript/精通/demo/dist/01-getting-started.js
+++
@@ -1,5 +0,0 @@
-const hello = (name) => {
-    console.log(`Hello, ${name}`);
-};
-hello('TypeScript');
-//# sourceMappingURL=01-getting-started.js.map
--- TypeScript/精通/demo/dist/04-object-types.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// Object 类型
-Object.defineProperty(exports, "__esModule", { value: true });
-const foo = function () { }; // FN, [], {}
-const obj = { foo: 123, bar: 'string' };
-//# sourceMappingURL=04-object-types.js.map
